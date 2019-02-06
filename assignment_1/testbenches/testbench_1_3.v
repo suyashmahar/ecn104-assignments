1474efd7d6326443acb172644bad01f154d65c5d
Suyash Mahar <suyash12mah.uec2016@iitr.ac.in>
2019-02-06 11:58:10 +0530

    Fix testbench for module_1_3

--- assignment_1/testbenches/testbench_1_3.v
+++ assignment_1/testbenches/testbench_1_3.v
@@ -1,6 +1,20 @@
+// 
+// Testbench for parityBits defined in module_1_3 
+// 
+// History:
+// * Wed Feb  6 09:51:58 IST 2019: Changes:
+//   * Fixed checks for unknown and high impedance states in 
+//     the result
+//   * Results are now verified on change of input and not 
+//     even and odd parity 
+// * Wed Feb 11 02:48:49 IST 2018:
+//   * First edition
+//
+// Copyright (c) 2018-19 Suyash Mahar <suyash12mahar@outlook.com>
+
 `timescale 1ns/1ps
 
-module testbench_1_2_1;
+module testbench_1_3_1;
    wire evenParity, oddParity;
    reg [31:0] inputSignal;
    
@@ -20,20 +34,26 @@ module testbench_1_2_1;
 
    always #10 inputSignal = inputSignal + 32'h00000111;
    
-   always @(evenParity, oddParity) begin
-       if ((evenParity != ^inputSignal) && (oddParity != ~^inputSignal)) begin
+   always @(inputSignal) begin
+       if ((evenParity != ^inputSignal) 
+       || (oddParity != ~^inputSignal) 
+	   || (^evenParity === 1'bx) 
+	   || (^oddParity === 1'bx)) begin
 	   error = 1'b1;
 	   $display("Testcase for input: %b failed, output -> evenParity: %b, oddParity: %b, expected-> evenParity: %b, oddParity: %b.", inputSignal, evenParity, oddParity, ^inputSignal, ~^inputSignal);
+	   $finish;
        end
    end
 
    always @(inputSignal) begin
        if (inputSignal >= 32'h0fff_ffff) begin
+	   $display("\n\n**************************************");
 	   if (error == 1'b1) begin
 	       $display("Testcase(s) failed!");
 	   end else begin
 	       $display("All testcases passed!");
 	   end
+	   $display("**************************************\n\n");
 	   $finish;
        end
    end
