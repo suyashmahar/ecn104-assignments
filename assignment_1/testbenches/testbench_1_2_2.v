956feb838a848bbdc0c0fa8657f786f286805dea
Suyash Mahar <suyash12mah.uec2016@iitr.ac.in>
2019-02-06 10:31:52 +0530

    Fix testbench for module_1_2*

--- assignment_1/testbenches/testbench_1_2_2.v
+++ assignment_1/testbenches/testbench_1_2_2.v
@@ -1,3 +1,17 @@
+// 
+// Testbench for nandGate defined in module_1_2_2
+// 
+// History:
+// * Wed Feb  6 09:57:49 IST 2019: Changes:
+//   * Fixed checks for unknown and high impedance states in 
+//     the result
+//   * Results are now verified on change of input and not 
+//     inputReg
+// * Wed Feb 11 02:48:49 IST 2018:
+//   * First edition
+//
+// Copyright (c) 2018-19 Suyash Mahar <suyash12mahar@outlook.com>
+
 `timescale 1ns/1ps
 
 module testbench_1_2_2;
@@ -20,18 +34,20 @@ module testbench_1_2_2;
        inputReg = inputReg + 2'b01;
        #25
 	 if (inputReg == 2'b11) begin
+	     $display("\n\n**************************************");
 	     if (error == 1'b0) begin
 		 $display("Test completed successfuly, all testcases passed.");
 	     end  else begin
 		 $display("Test cases failed!");
 	     end
+	     $display("**************************************\n\n");
 	     $finish;
 	 end
    end
 
 
-   always @(result) begin
-       if (result != &inputReg) begin
+   always @(inputReg) begin
+       if ((result != &inputReg) || (^result === 1'bx)) begin
 	   error = 1'b1;
 	   $display("Testcase for input: %b, %b failed, output: %b, expected: %b", inputReg[0], inputReg[1], result, &inputReg);
        end
